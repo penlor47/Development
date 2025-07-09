2eea75f8e1739aab3732987594583eb3336dddbf
Penelope Lorrimore <penlor23@gmail.com>
2025-07-09 16:36:16 +0100

    Complete calculator for two number calculation. = still needs to be pressed between each number.

---
+++ Learning/AppBrewery/Calculator-Advanced-Swift-iOS13/Calculator/Controller/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        // Turning the input string into a double
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Number in the display is not a number.")
+            }
+            return number
+        }
+        // Turning the input double into a string
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calculator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            // we use 'guard let' rather than an if let when you are guarding against something terrible and the else will be rarely triggered.
+            // an 'if let' is that most of the time it will be nil, but sometimes it will be wrong and if that happens we can just ignore those cases.
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+}
+
